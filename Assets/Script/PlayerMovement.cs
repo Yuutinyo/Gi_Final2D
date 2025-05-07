3e3aa772bce3ff4b7430f7bcd1eafdb7307fb0a8
Yuutinyo <yuunapat.7482@gmail.com>
2025-05-07 18:33:00 +0700

    ใส่ตัว

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,15 +5,16 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float jumpForce;
     float inputMovement;
-    Rigidbody2D rb;
+    Rigidbody2D rb2d;
     bool isGrounded;
     public Transform groundCheck;
     public LayerMask groundMask;
+    //Vector2 move;
     
     public float groundCheckRadius;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,13 +23,15 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
         
         inputMovement = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(inputMovement * speed, rb.linearVelocity.y);
+        rb2d.linearVelocity = new Vector2(inputMovement * speed, rb2d.linearVelocity.y);
     }
     void Update()
     {
+        //move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //rb2d.AddForce(move * speed);
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = Vector2.up * jumpForce;
+            rb2d.linearVelocity = Vector2.up * jumpForce;
         }
     }
 }
